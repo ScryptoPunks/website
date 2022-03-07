a5e69b8a8c25a1e232668b168c08cf019bdeff3a
nipud0s <nipudos-01@yahoo.com>
2022-03-07 18:03:29 +0000

    NFT page layout

--- src/components/NFT/index.js
+++ src/components/NFT/index.js
@@ -3,13 +3,17 @@ import { useParams } from "react-router-dom";
 import "./index.css";
 
 export default function NFT() {
-    const [attributes, setAttributes] = useState([]);
+    const [attributes, setAttributes] = useState();
+    const [rarity, setRarity] = useState();
     const { id } = useParams();
 
     useEffect(() => {
         fetch(`/json/${id}.json`)
             .then((res) => res.json())
             .then((json) => setAttributes(json["attributes"]));
+        fetch("/json/_rarity.json")
+            .then((res) => res.json())
+            .then((json) => setRarity(json));
     }, [id]);
 
     return (
@@ -25,17 +29,33 @@ export default function NFT() {
                 </div>
             </div>
             <div className="right-panel">
-                {attributes.map((attribute, i) => (
-                    <div className="attribute" key={i}>
-                        <span className="trait-type">
-                            {attribute["trait_type"]}:
-                        </span>
-                        <div className="trait-value">
-                            <span>{attribute["value"]}</span>
-                            {/* <span class="rare rarity">Rare (16.79%)</span> */}
+                {attributes && rarity && attributes.map((attribute, i) => {
+                    const layer = attribute["trait_type"];
+                    const trait = attribute["value"];
+                    const occurence = rarity[layer][trait];
+                    
+                    let category;
+                    if (occurence < 100) category = "mythic";
+                    else if (occurence < 500) category = "legendary";
+                    else if (occurence < 1000) category = "epic";
+                    else if (occurence < 2500) category = "rare";
+                    else if (occurence < 5000) category = "uncommon";
+                    else category = "common";
+
+                    return (
+                        <div className="attribute" key={i}>
+                            <span className="trait-type">
+                                {attribute["trait_type"]}:
+                            </span>
+                            <div className="trait-value">
+                                <span>{attribute["value"]}</span>
+                                <span className={`rarity ${category}`}>
+                                    {category} ({occurence / 100}%)
+                                </span>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
     );
