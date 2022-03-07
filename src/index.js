8355f6b3b60c02e6415e4d134559bdf0a3d0a7d8
nipud0s <nipudos-01@yahoo.com>
2022-03-07 18:56:43 +0000

    Remove useless NFTs component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Collection from "./components/Collection";
-import NFTs from "./components/NFTs";
 import NFT from "./components/NFT";
 import ErrorPage from "./components/ErrorPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -14,9 +13,7 @@ ReactDOM.render(
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route path="collection" element={<Collection />}></Route>
-                    <Route path="nfts" element={<NFTs />}>
-                        <Route path=":id" element={<NFT />}></Route>
-                    </Route>
+                    <Route path="nfts/:id" element={<NFT />}></Route>
                     <Route path="*" element={<ErrorPage />}></Route>
                 </Route>
             </Routes>
