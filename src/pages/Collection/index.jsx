574b06ee46fa933c0e0a8c3bfb13d18f96110ebf
n1pu <nipudos-01@yahoo.com>
2022-06-07 01:43:14 +0200

    Add search by wallet & sort by rank/random

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Mosaic from "../../components/Mosaic";
 import Select from "react-select";
-import { repo } from "../../config";
+import { api, repo } from "../../config";
 import "./index.css";
 
 export default function App() {
     const [data, setData] = useState([]);
     const [layers, setLayers] = useState([]);
     const [options, setOptions] = useState([]);
-    const [filters, setFilters] = useState([
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-    ]);
+    const [filters, setFilters] = useState([[], [], [], [], [], [], []]);
     const [range, setRange] = useState(100);
+    const [wallet, setWallet] = useState();
+    const [sort, setSort] = useState("Ascending");
+    const [database, setDatabase] = useState();
+    const [ranks, setRanks] = useState();
 
     const handleChange = (selected, index) => {
         let copy = [...filters];
@@ -29,6 +25,21 @@ export default function App() {
 
     const checkValid = (arr, val) => {
         return arr.includes(val) || arr.length === 0;
+    };
+
+    const sortNFTs = (data) => {
+        if (sort === "Random") {
+            return data
+                .map((value) => ({ value, sort: Math.random() }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({ value }) => value);
+        } else if (sort === "Rank") {
+            return Object.entries(ranks)
+                .sort(([, a], [, b]) => a - b)
+                .map((entry) => {
+                    return { edition: parseInt(entry[0]) };
+                });
+        } else return data;
     };
 
     useEffect(() => {
@@ -43,6 +54,12 @@ export default function App() {
         fetch(`${repo}/_metadata.json`)
             .then((res) => res.json())
             .then((json) => setData(json));
+        fetch(`${api}/database.json`)
+            .then((res) => res.json())
+            .then((json) => setDatabase(json));
+        fetch(`${repo}/ranks.json`)
+            .then((res) => res.json())
+            .then((json) => setRanks(json));
     }, []);
 
     return (
@@ -72,79 +89,111 @@ export default function App() {
                     ))}
                 </div>
                 <div className="count">
-                    {
-                        data.filter((nft) => {
-                            return (
-                                checkValid(
-                                    filters[0],
-                                    nft.attributes[0].value
-                                ) &&
-                                checkValid(
-                                    filters[1],
-                                    nft.attributes[1].value
-                                ) &&
-                                checkValid(
-                                    filters[2],
-                                    nft.attributes[2].value
-                                ) &&
-                                checkValid(
-                                    filters[3],
-                                    nft.attributes[3].value
-                                ) &&
-                                checkValid(
-                                    filters[4],
-                                    nft.attributes[4].value
-                                ) &&
-                                checkValid(
-                                    filters[5],
-                                    nft.attributes[5].value
-                                ) &&
-                                checkValid(
-                                    filters[6],
-                                    nft.attributes[6].value
-                                )
-                            );
-                        }).length
-                    }{" "}
+                    {wallet
+                        ? Object.entries(database).filter(
+                              ([key, value]) => value === wallet
+                          ).length
+                        : data.filter((nft) => {
+                              return (
+                                  checkValid(
+                                      filters[0],
+                                      nft.attributes[0].value
+                                  ) &&
+                                  checkValid(
+                                      filters[1],
+                                      nft.attributes[1].value
+                                  ) &&
+                                  checkValid(
+                                      filters[2],
+                                      nft.attributes[2].value
+                                  ) &&
+                                  checkValid(
+                                      filters[3],
+                                      nft.attributes[3].value
+                                  ) &&
+                                  checkValid(
+                                      filters[4],
+                                      nft.attributes[4].value
+                                  ) &&
+                                  checkValid(
+                                      filters[5],
+                                      nft.attributes[5].value
+                                  ) &&
+                                  checkValid(
+                                      filters[6],
+                                      nft.attributes[6].value
+                                  )
+                              );
+                          }).length}{" "}
                     NFTs found
+                </div>
+                <div className="search-container">
+                    Search by wallet:
+                    <input
+                        type="text"
+                        placeholder="Enter your address"
+                        onChange={(e) => setWallet(e.currentTarget.value)}
+                    />
+                </div>
+                <div className="sort-container">
+                    Sort by:
+                    <select onChange={(e) => setSort(e.currentTarget.value)}>
+                        <option value="Ascending">Ascending</option>
+                        <option value="Rank">Rank</option>
+                        <option value="Random">Random</option>
+                    </select>
                 </div>
             </div>
             {data && (
                 <Mosaic
-                    data={data
-                        .filter((nft) => {
-                            return (
-                                checkValid(
-                                    filters[0],
-                                    nft.attributes[0].value
-                                ) &&
-                                checkValid(
-                                    filters[1],
-                                    nft.attributes[1].value
-                                ) &&
-                                checkValid(
-                                    filters[2],
-                                    nft.attributes[2].value
-                                ) &&
-                                checkValid(
-                                    filters[3],
-                                    nft.attributes[3].value
-                                ) &&
-                                checkValid(
-                                    filters[4],
-                                    nft.attributes[4].value
-                                ) &&
-                                checkValid(
-                                    filters[5],
-                                    nft.attributes[5].value
-                                ) &&
-                                checkValid(
-                                    filters[6],
-                                    nft.attributes[6].value
-                                )
-                            );
-                        })
-                        .slice(0, range)}
+                    data={
+                        wallet
+                            ? sortNFTs(
+                                  Object.entries(database)
+                                      .filter(
+                                          ([key, value]) => value === wallet
+                                      )
+                                      .map((entry) => {
+                                          return {
+                                              edition: parseInt(entry[0]),
+                                          };
+                                      })
+                              ).slice(0, range)
+                            : sortNFTs(
+                                  data.filter((nft) => {
+                                      return (
+                                          checkValid(
+                                              filters[0],
+                                              nft.attributes[0].value
+                                          ) &&
+                                          checkValid(
+                                              filters[1],
+                                              nft.attributes[1].value
+                                          ) &&
+                                          checkValid(
+                                              filters[2],
+                                              nft.attributes[2].value
+                                          ) &&
+                                          checkValid(
+                                              filters[3],
+                                              nft.attributes[3].value
+                                          ) &&
+                                          checkValid(
+                                              filters[4],
+                                              nft.attributes[4].value
+                                          ) &&
+                                          checkValid(
+                                              filters[5],
+                                              nft.attributes[5].value
+                                          ) &&
+                                          checkValid(
+                                              filters[6],
+                                              nft.attributes[6].value
+                                          )
+                                      );
+                                  })
+                              ).slice(0, range)
+                    }
                 />
             )}
             <div
